501ff2158d338ef54ae1cc66dde028533a7ddde1
Manimaran <r.manimaran@yahoo.co.in>
2024-03-06 21:33:41 -0500

    Add project files.

---
+++ LuceneSearchDotnet/PersonSearchEngine.cs
@@ -0,0 +1,76 @@
+﻿using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.QueryParsers.Classic;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+using Lucene.Net.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuceneSearchDotnet;
+
+public class PersonSearchEngine
+{
+    const LuceneVersion _version = LuceneVersion.LUCENE_48;
+    private readonly StandardAnalyzer _analyzer;
+    private readonly RAMDirectory _directory;
+    private readonly IndexWriter _writer;
+
+    public PersonSearchEngine()
+    {
+        _analyzer = new StandardAnalyzer(_version);
+        _directory = new RAMDirectory();
+        var config = new IndexWriterConfig(_version, _analyzer);
+        _writer = new IndexWriter(_directory, config);
+    }
+
+
+    public void AddPersonsToIndex(IEnumerable<Person> persons)
+    {
+        foreach (var person in persons)
+        {
+            var document = new Document();
+            document.Add(new StringField("Id", person.Id.ToString(), Field.Store.YES));
+            document.Add(new TextField("FirstName", person.FirstName, Field.Store.YES));
+            document.Add(new TextField("LastName", person.LastName, Field.Store.YES));
+            document.Add(new TextField("Title", person.Title, Field.Store.YES));
+            document.Add(new TextField("Email", person.Email, Field.Store.YES));
+            document.Add(new TextField("Company", person.Company, Field.Store.YES));
+            document.Add(new TextField("Description", person.Description, Field.Store.YES));
+            _writer.AddDocument(document);
+        }
+        _writer.Commit();
+    }
+
+    public IEnumerable<Person> Search(string searchTerm)
+    {
+        var directoryReader = DirectoryReader.Open(_directory);
+        var indexSearcher = new IndexSearcher(directoryReader);
+
+        string[] fields = { "FirstName", "LastName", "Title", "Email", "Company", "Description" };
+        var queryParser = new MultiFieldQueryParser(_version, fields,_analyzer);
+        var query = queryParser.Parse(searchTerm);
+        var hits = indexSearcher.Search(query, 10).ScoreDocs;
+        var persons = new List<Person>();
+        foreach(var person in hits)
+        {
+            var document = indexSearcher.Doc(person.Doc);
+            persons.Add(new Person()
+            {
+                Id = new Guid(document.Get("Id")),
+                FirstName = document.Get("FirstName"),
+                LastName = document.Get("LastName"),
+                Title = document.Get("Title"),
+                Email = document.Get("Email"),
+                Company = document.Get("Company"),
+                Description = document.Get("Description")
+            }); 
+        }
+        return persons;
+    }
+}
---
+++ LuceneSearchDotnet/Program.cs
@@ -0,0 +1,36 @@
+﻿using LuceneSearchDotnet;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using newtonsoft =Newtonsoft.Json;
+var personFile = File.ReadAllText("PersonData.json");
+var persons = JsonSerializer.Deserialize<List<Person>>(personFile, 
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive =true});
+
+var engine = new PersonSearchEngine();
+if (persons == null) return;
+
+engine.AddPersonsToIndex(persons);
+
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("Enter a Search query:");
+    var query = Console.ReadLine();
+    if(string.IsNullOrEmpty(query)) return;
+
+    var results = engine.Search(query);
+    if(results == null || results.Count() == 0)
+    {
+        Console.WriteLine("No Results found");
+        continue;
+    }
+
+    Console.WriteLine("Results");
+    foreach(var person in results)
+    {
+        Console.WriteLine(newtonsoft.JsonConvert.SerializeObject(person,newtonsoft.Formatting.Indented));
+    }
+    Console.WriteLine("Press any key to continue");
+    Console.ReadKey();
+}
